d4ce5104a055ef9b2ad28a792bf803c26287a6a2
Hesham ElAbd <HeshamElAbd>
2022-02-02 12:18:05 +0100

    updating the code

--- src/functions.rs
+++ src/functions.rs
@@ -11,9 +11,27 @@ use bincode::{serialize_into,deserialize_from};
 use std::io::BufWriter;
 
 /// ### Summary
-/// 
-/// 
-#[inline]
+/// Create proteins in a specific tissue
+/// ### Parameters
+/// target_protein: A vector of string representing protein names 
+/// tissue_name: A string representing the tissue name 
+/// annoTable: The annotation table database which is a hash map with the following structure
+///     tissue_name --> HashMap:
+///                     ProteinName --> ProteinInfo 
+///                                         |__subcellular_location 
+///                                         |__d2g
+///                                         |__expression 
+/// ### Notes
+/// 1. The function will panic if the tissue name did not match any tissue defined in the database, also match is CASE-SENSITIVE
+/// 2. The function uses Rayon to execute the performance across multiple threads
+/// ### Returns
+/// A hashmap with the following structure
+///             String-->ProteinInfo
+///                             |__subcellular_location 
+///                             |__d2g
+///                             |__expression
+///  where the string is the name of the protein and the ProteinInfo is annotation for the provided protein in the target tissue  
+#[inline(always)]
 pub fn annotate_proteins_from_a_tissue(target_protein:&Vec<String>,tissue_name:String,
         annoTable:&HashMap<String,HashMap<String,ProteinInfo>>)->Result<HashMap<String,ProteinInfo>,String>
 {
@@ -34,35 +52,72 @@ pub fn annotate_proteins_from_a_tissue(target_protein:&Vec<String>,tissue_name:S
                 }; 
                 (protein_id.clone(),target_protein_info)
             })
-        .filter(|(protein_name,protin_info)|protin_info.get_sub_cellular_loc()!="?")
+        .filter(|(_,protin_info)|protin_info.get_sub_cellular_loc()!="?")
         .collect::<HashMap<_,_>>())
 }
 
-
-pub fn annotate_proteins_in_tissues(target_protein:&Vec<(String,String)>,tissue_name:String,
-    annoTable:&HashMap<String,HashMap<String,ProteinInfo>>)->HashMap<String,ProteinInfo>
+/// ### Summary
+/// Annotate proteins in different tissues 
+/// ### Parameters
+/// 1. target_protein ==> A vector of tuple each is composite of two strings, the first is the protein-id and the second is the tissue name. 
+///     Thus, the input structure is: tuple 1, tuple 2, ...., tuple n. Where each tuple is composite of:
+///         a. protein_id --> a string representing protein-id
+///         b. tissue_name --> a string representing the name of the tissue
+/// 2. annoTable: The annotation table database which is a hash map with the following structure
+///     tissue_name --> HashMap:
+///                     ProteinName --> Tuple
+///                                         |__ ProteinInfo
+///                                         |    |__subcellular_location 
+///                                         |    |__d2g
+///                                         |__tissue_name
+/// ### Returns
+/// A hashmap with the following structure
+///             String-->ProteinInfo
+///                             |__subcellular_location 
+///                             |__d2g
+///                             |__expression
+///  where the string is the name of the protein and the ProteinInfo is annotation for the provided protein in the target tissue, 
+/// ### Note
+/// 1. If the tissue name in one of the tuples is not defined, then the input tuple is skipped, e.g. vec![("PROT1".to_string(),"Tissue1".to_string()),
+/// ("PROT2".to_string(),"Tissue2".to_string())] & if tissue two, i.e. Tissue2 is not in the database then the input tuple is cleared. 
+/// 2. the function uses a pool of Rayon Threads to execute the code in parallel. 
+pub fn annotate_proteins_in_tissues(target_protein:Vec<(String,String)>,
+    annoTable:&HashMap<String,HashMap<String,ProteinInfo>>)->HashMap<String,(ProteinInfo,String)>
 {
     target_protein
-        .par_iter()
-        .filter(|(protein_name,tissue_name)|annoTable.contains_key(tissue_name))
+        .into_par_iter()
+        .filter(|(_,tissue_name)|annoTable.contains_key(tissue_name))
         .map(|(protein_id,tissue_name)|
             {
-                let target_protein_info=match annoTable.get(tissue_name).unwrap().get(protein_id)
+                let target_protein_info=match annoTable.get(&tissue_name).unwrap().get(&protein_id)
                 {
                     Some(res)=>res.clone(),
                     None=>ProteinInfo::new("?".to_string(),"?".to_string(),0.0) // Default protein then it can be filtered out in the end 
                 }; 
-                (protein_id.clone(),target_protein_info)
+                (protein_id.clone(),(target_protein_info,tissue_name))
             })
-            .filter(|(protein_name,protein_info)|protein_info.get_sub_cellular_loc()!="?")
+            .filter(|(_,(protein_info,_))|protein_info.get_sub_cellular_loc()!="?")
             .collect::<HashMap<_,_>>()
 }
 
+/// ### Summary
+/// A convenient wrapper function used to automate the task of parsing the annotation table and generating the annotations for a collection of proteins 
+/// in a specific tissue. 
+/// ### Parameters
+/// 1. path --> The path to load the annotation table 
+/// 2. target_protein ---> The target proteins, which a vector of strings containing the name of proteins
+/// 3. tissue --> a string representing the tissue name  
+/// ### Returns 
+/// A vector of tuples each of which is composite of 4 elements:
+///     1. A string representing the protein name, i.e. uniprot ID 
+///     1. A String representing the subcellular location
+///     2. A String representing the distance to glycosylation 
+///     3. A float representing the gene expression 
 pub fn annotate_all_proteins_in_one_tissue(path:&Path,
-    target_protein:&Vec<String>,tissue_name:String)->Result<Vec<(String,String,String,f32)>,String>
+    target_protein:&Vec<String>,tissue_name:String)->Vec<(String,String,String,f32)>
 {
     let database=AnnotationTable::read_table(path).to_hashmap();
-    Ok(annotate_proteins_from_a_tissue(target_protein,tissue_name,&database)
+    annotate_proteins_from_a_tissue(target_protein,tissue_name,&database)
         .unwrap()
         .into_par_iter()
         .map(|(protein_name, protein_info)|
@@ -70,10 +125,21 @@ pub fn annotate_all_proteins_in_one_tissue(path:&Path,
             (protein_name,protein_info.get_sub_cellular_loc().clone(),protein_info.get_d2g().clone(),protein_info.get_expression())
         })
         .collect::<Vec<_>>()
-    )
 }
-
- 
+/// ### Summary
+/// A convenient wrapper function used to automate the task of parsing the annotation table and generating the annotations for a collection of proteins 
+/// in a specific tissue. This used function used a pre-computed and cashed database for a faster execution speed. For more information check the function: 
+/// cash_database_to_disk defined in the current modules.    
+/// ### Parameters
+/// 1. path --> The path to load the annotation table 
+/// 2. target_protein ---> The target proteins, which a vector of strings containing the name of proteins
+/// 3. tissue --> A string representing the tissue name  
+/// ### Returns
+/// A vector of tuples each of which is composite of 4 elements:
+///     1. A string representing the protein name, i.e. uniprot ID 
+///     1. A String representing the subcellular location
+///     2. A String representing the distance to glycosylation 
+///     3. A float representing the gene expression 
 pub fn annotate_all_proteins_in_one_tissues_using_cashed_db(path2cashed_db:&Path,
     target_protein:&Vec<String>,tissue_name:String)->Result<Vec<(String,String,String,f32)>,String>
 {
@@ -90,16 +156,25 @@ pub fn annotate_all_proteins_in_one_tissues_using_cashed_db(path2cashed_db:&Path
 }
 
 
+/// ### Summary
+/// takes the path to an annotation table, parse and create an hashmap out of it finally it write the hashmap to the disk using serde. 
+/// This enable the database to be reloaded relatively fast for subsequent usages. 
+/// ### Parameters
+///     1. path2file --> the path to the input annotation table 
+///     2. path2res --> the pash to write the serialized hashmap 
 pub fn cash_database_to_disk(path2file:&Path,path2res:&Path)->()
 {
     // load the database
     let database=AnnotationTable::read_table(path2file).to_hashmap();
     // create a file to hold the results
-    let mut f = BufWriter::new(File::create(path2res).unwrap());
+    let mut file_writer = BufWriter::new(File::create(path2res).unwrap());
     // serialize the results into the generated writer buffer 
-    serialize_into(&mut f, &database).unwrap();
+    serialize_into(&mut file_writer, &database).unwrap();
 }
-
+/// ### Summary 
+/// Load the Serialized database from the disk
+/// ### Parameters
+/// path2res --> The path to cashed database, see the function cash_database_to_disk for more information 
 pub fn read_cashed_db(path2res:&Path)->HashMap<String,HashMap<String,ProteinInfo>>
 {
     // open the file
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,12 @@
 /// A collection of function used for parsing and preparing the input
+/// 
+/// 
+/// 
+/// 
 use std::{collections::HashMap, path::Path};
+use rayon::prelude::*;
 use crate::functions::*; 
-use ndarray::Dim;
+use ndarray::{Dim, Array};
 use pyo3::prelude::*;
 use crate::peptides::*; 
 use numpy::{ToPyArray, PyArray};
@@ -13,47 +18,216 @@ pub mod peptides;
 pub mod expression_db; 
 
 
-
+/// ### Signature
+/// annotate_proteins(path:str,target_protein:List[str],tissue_name:str)->List[Tuple[str,str,str,float32]]
+/// ### Summary
+/// A wrapper function used to automate the task of parsing the annotation table and generating the annotations for a collection of proteins 
+/// in a specific tissue.
+/// ### Executioner 
+/// This function is a wrapper function for the Rust function annotate_all_proteins_in_one_tissue, for more details regard the execution logic check 
+/// the documentation for this function.
+/// ### Parameters
+/// path: The path to load the annotation table
+/// target_protein: A list of target proteins to annotate
+/// tissue_name: The name of the tissue where annotation information should be extracted
+/// ### Returns
+/// A list of tuples each of which is composite of 4 elements:
+///     1. A string representing the protein name, i.e. uniprot ID 
+///     1. A String representing the subcellular location
+///     2. A String representing the distance to glycosylation 
+///     3. A float representing the gene expression 
+/// --
 #[pyfunction]
-fn annotate_proteins(path:String,target_protein:Vec<String>,
+fn annotate_proteins(path:String,target_proteins:Vec<String>,
     tissue_name:String)->PyResult<Vec<(String,String,String,f32)>>
 {
-    Ok(annotate_all_proteins_in_one_tissue(&Path::new(&path),&target_protein,tissue_name).unwrap())
+    Ok(annotate_all_proteins_in_one_tissue(&Path::new(&path),&target_proteins,tissue_name))
     
 }
-
+/// ### Signature
+///  cash_database(path2db:str, path2res:str)-> None
+/// ### Summary
+/// takes the path to an annotation table, parse and create an hashmap out of it finally it write the hashmap to the disk using serde.
+///  This enable the database to be reloaded relatively fast for subsequent usages.
+/// ### Executioner 
+/// This function is a wrapper function for the Rust function cash_database_to_disk, for more details regard the execution logic check 
+/// the documentation for this function.
+/// ### Parameters
+/// path2db: The path to load the database, i.e. the annotation table 
+/// path2res: The path to write the results, i.e. the cashed database
+/// --
 #[pyfunction]
 fn cash_database(path2db:String,path2res:String)->PyResult<()>
 {
     Ok(cash_database_to_disk(&Path::new(&path2db), &Path::new(&path2res)))
 }
-
+/// ### Signature
+/// annotate_proteins_using_cashed_db(path:str,target_protein:List[str],tissue_name:str)->List[Tuple[str,str,str,float32]]
+/// ### Summary
+/// A wrapper function used to automate the task of parsing the annotation table and generating the annotations for a collection of proteins 
+/// in a specific tissue.
+/// ### Executioner 
+/// This function is a wrapper function for the Rust function annotate_all_proteins_in_one_tissues_using_cashed_db, for more details regard the execution logic check 
+/// the documentation for this function.
+/// ### Parameters
+/// path: The path to load the *CASHED* annotation table
+/// target_protein: A list of target proteins to annotate
+/// tissue_name: The name of the tissue where annotation information should be extracted
+/// ### Returns
+/// A list of tuples each of which is composite of 4 elements:
+///     1. A string representing the protein name, i.e. uniprot ID 
+///     1. A String representing the subcellular location
+///     2. A String representing the distance to glycosylation 
+///     3. A float representing the gene expression 
+/// --
 #[pyfunction]
 fn annotate_proteins_using_cashed_db(path2cashed_db:String, target_protein:Vec<String>,
     tissue_name:String)->PyResult<Vec<(String,String,String,f32)>>
 {
     Ok(annotate_all_proteins_in_one_tissues_using_cashed_db(&Path::new(&path2cashed_db),&target_protein,tissue_name).unwrap())
-    
 }
 
+
+#[pyfunction]
+fn get_annotated_protein_arrays<'py>(py:Python<'py>,
+        path2cashed_db:String, target_protein:Vec<String>,
+        tissue_name:String)->(Vec<String>,
+            &'py PyArray<u8,Dim<[usize;2]>>,
+            &'py PyArray<u16,Dim<[usize;2]>>,
+            &'py PyArray<f32,Dim<[usize;2]>>
+     )
+{
+    // build the annotations 
+    //-----------------------
+    let vec_annotation=annotate_all_proteins_in_one_tissues_using_cashed_db( &Path::new(&path2cashed_db),&target_protein,tissue_name).unwrap(); 
+    // allocate the arrays
+    let mut protein_ids=Vec::with_capacity(vec_annotation.len());
+    let mut sub_cell_location=Vec::with_capacity(vec_annotation.len()); 
+    let mut distance2glyco=Vec::with_capacity(vec_annotation.len()); 
+    let mut gene_exp=Vec::with_capacity(vec_annotation.len()); 
+    // fill the vectors 
+    //-----------------
+    for (protein_id,subcell_loc,d2g,exp) in vec_annotation
+    {
+        protein_ids.push(protein_id);
+        sub_cell_location.push(subcell_loc);
+        distance2glyco.push(distance2glyco); 
+        gene_exp.push(exp); 
+    }
+    // calling the encoders
+    let encoded_subcell_loc=encode_subcell_locations();
+    let encoded_d2g=encode_distance_to_glyco(); 
+    let gene_exp=encode_gene_expression(); 
+}
+
+
+
+#[pyfunction]
+fn prepare_training_data()
+
+
+
+
+
+/// ### Signature
+/// group_peptide_by_protein(peptide:List[str],proteome:Dict[str,str])->Dict[str,List[str]]
+/// ### Summary
+/// The function takes a list of peptides as an input and a dict of protein names mapped to protein sequences as values and return a dict containing
+/// the peptides as keys and list of protein ids where the proteins were observed as values.
+/// ### Executioner 
+/// This function is a wrapper function for the Rust function group_peptide_by_protein, for more details regard the execution logic check 
+/// the documentation for this function.
+/// ### Parameters
+/// peptides: A list of peptide sequences
+/// proteome: A dict of protein-name as keys and proteins-sequences as values 
+/// ### Returns
+/// a dict containing the peptides as keys and list of protein ids where the proteins were observed as values.   
+///---- 
 #[pyfunction]
 fn group_peptide_by_protein(peptides:Vec<String>,proteome:HashMap<String,String>)->PyResult<HashMap<String,Vec<String>>>
 {
     Ok(group_peptides_by_parent_rs(peptides,proteome))
 }
-
+/// ### Signature
+/// group_by_9mers(peptide:List[str])->Dict[str,List[str]]
+/// ### Summary
+/// group input peptide by the 9 mers cores, the function first create a set of all unique 9 mers in the input list of peptides afterword
+/// it loops over all peptides and create a dict with 9mers are keys and a collection of peptide sequences where the 9 mers was observed as a value.
+/// ### Executioner 
+/// This function is a wrapper function for the Rust function group_by_9mers_rs, for more details regard the execution logic check 
+/// the documentation for this function.
+/// ### Parameters
+/// peptides: A list of peptide sequences
+/// ### Returns
+/// a dict containing the peptides as keys and and a list of peptide sequences where the 9 mers was observed as a value.  
+/// 
 #[pyfunction]
 fn group_by_9mers(peptides:Vec<String>)->PyResult<HashMap<String,Vec<String>>>
 {
     Ok(group_by_9mers_rs(peptides))
 }
 
-
+/// ### Signature
+/// generate_a_train_db_by_shuffling(positive_examples:List[str], fold_neg:int)->Tuple[List[str],List[str]]
+/// ### Summary
+/// create a train database from a collection of positive examples, where the function first reads an input list of positive examples
+/// create a matched database of negative examples through shuffling and return a tuple of two lists, the first contain peptide sequences 
+/// while the second contain numerical labels.  
+/// ### Executioner 
+/// This function is a wrapper function for the Rust function generate_a_train_db_by_shuffling_rs, for more details regard the execution logic check 
+/// the documentation for this function.
+/// ### Parameters
+/// positive_examples: A list of peptide sequences representing positive examples
+/// fold_neg: The ration of the negative, i.e. shuffled generated, to the positive examples. 
+/// ### Returns
+/// a tuple of two lists, the first contain peptide sequences while the second contain numerical labels.  
 #[pyfunction]
 fn generate_a_train_db_by_shuffling(positive_examples:Vec<String>, fold_neg:u32)->PyResult<(Vec<String>,Vec<u8>)>
 {
     Ok(generate_a_train_db_by_shuffling_rs(positive_examples,fold_neg))
 }
+
+/// ### Signature
+/// generate_a_train_arrays_by_shuffling(positive_examples:List[str], fold_neg:int, max_len:int)->Tuple[Lnp.ndarray,np.ndarray]
+/// ### Summary
+/// Takes a list of positive examples, samples an x fold of negative examples through shuffling where x is determined through the parameters fold_neg
+/// and returns two arrays the first represent the encoded representation for input peptides while the second represent the label of each peptide in the database. 
+/// ### Executioner 
+/// This function is a wrapper function for two main Rust functions: generate_a_train_db_by_shuffling_rs and encode_sequence_rs, for more details regard the execution logic check 
+/// the documentation for these functions.
+/// ### Parameters
+/// positive_examples: A list of peptide sequences representing positive examples
+/// fold_neg: The ration of the negative, i.e. shuffled generated, to the positive examples, e.g. 1 means one positive to 1 negative while 10 means 1 positive to 10 negatives.
+/// max_len: The maximum peptide length, used for padding, longer peptides are trimmed to this length and shorter are zero-padded.   
+/// ### Results
+/// A tuple of two arrays, the first represents encoded peptide sequences which has a shape of (num_peptides, max_length) as a type of u8 while the second array
+/// represent the numerical labels of each peptides, 1 represent positive peptides and 0 represent negative peptides. This array has a shape of (num_peptides,1) and u8 as types. 
+/// 
+#[pyfunction]
+fn generate_a_train_arrays_by_shuffling<'py>(py:Python<'py>,
+        positive_examples:Vec<String>,
+        fold_neg:u32, max_len:usize)->(&'py PyArray<u8,Dim<[usize;2]>>,&'py PyArray<u8,Dim<[usize;2]>>)
+{
+    // create the database which is made from the sequences and the labels
+    let (generated_seq, labels) = generate_a_train_db_by_shuffling_rs(positive_examples,fold_neg);
+
+    // numerical encode the database
+    let encoded_seq = encode_sequence_rs(generated_seq,max_len).to_pyarray(py);
+    
+    // create an array from the labels
+    let labels = Array::from_shape_vec((encoded_seq.shape()[0],1), labels).unwrap().to_pyarray(py);
+
+    // return the results
+    (encoded_seq,labels)
+}
+
+/* 
+#[pyfunction]
+fn generate_a_train_arrays_by_proteome_sampling<'py>(py:Python<'py>,
+            positive_examples:Vec<String>,fold_neg:u32, max_len:usize)->(&'py PyArray<u8,Dim<[usize;2]>>,&'py PyArray<u8,Dim<[usize;2]>>)
+{
+
+}*/
 
 /// encode_sequence(seq:List[str],max_len:int)->np.ndarray
 /// --
@@ -66,12 +240,10 @@ fn encode_sequence<'py>(py:Python<'py>,seq:Vec<String>,max_len:usize)->&'py PyAr
     encode_sequence_rs(seq,max_len).to_pyarray(py)
 }
 
-
-
-
 /// A Python module implemented in Rust.
 #[pymodule]
-fn RustDB(_py: Python, m: &PyModule) -> PyResult<()> {
+fn RustDB(_py: Python, m: &PyModule) -> PyResult<()> 
+{
     m.add_function(wrap_pyfunction!(annotate_proteins, m)?)?;
     m.add_function(wrap_pyfunction!(cash_database,m)?)?;
     m.add_function(wrap_pyfunction!(annotate_proteins_using_cashed_db,m)?)?;
@@ -79,5 +251,6 @@ fn RustDB(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(group_by_9mers,m)?)?; 
     m.add_function(wrap_pyfunction!(generate_a_train_db_by_shuffling,m)?)?;
     m.add_function(wrap_pyfunction!(encode_sequence,m)?)?;
+    m.add_function(wrap_pyfunction!(generate_a_train_arrays_by_shuffling,m)?)?;
     Ok(())
 }
