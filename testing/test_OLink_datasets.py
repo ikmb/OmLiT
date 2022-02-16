53ae151b2c464521d00b380e6a4e34ee1b7957ae
Hesham ElAbd <HeshamElAbd>
2022-02-16 17:45:38 +0100

    adding documentation to the function

---
+++ testing/test_OLink_datasets.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python 
+"""
+@author: Hesham ElAbd 
+@brief: analyzing the performance of the aspects of the library
+    1- Reading and parsing Quantitative data against the models 
+    2- generating data from multi-modal pure sequence models 
+"""
+## Loading the modules 
+#---------------------
+import pandas as pd 
+import numpy as np 
+import pandas 
+
+
+## Test the reading and parsing of the Q models 
+#----------------------------------------------
