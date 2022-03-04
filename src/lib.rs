c532a77b5a6d6a5caf92f2f5f971326893a179d0
Hesham ElAbd <HeshamElAbd>
2022-03-04 12:59:34 +0100

    updating the function code

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,8 @@
 /// Author: Hesham ElAbd 
 /// Contact: h.elabd@ikmb.uni-kiel.de
 /// Copyrights: Institute of clinical molecular biology, Kiel, Germany.
-/// Version: 0.1.0 pre-alpha
-/// Release data: 10.02.2022
+/// Version: 0.1.1 alpha
+/// Release data: 04.03.2022
 /// Initial Release date: 10.02.2022
 /// Rust Code: The functions in this module provide a thin wrapper a round the rust code which is used for executing all the heavy-lifting jobs.
 ///         To skip the current module and work with the Rust-code directly or to extend the rust code, check the code defined in the file omics_builder.rs.
@@ -13,36 +13,20 @@
 ///         incase of any bug, contact the developer using the contact information defined above or open an issue at the github page (https://github.com/ikmb/O-Link-).
 /// Change Track:
 ///     No update to the 0.1.0 pre-alpha version has been documented.
+///     Version 0.1.1:
+///         The following changes were included into the modules
+///             a. Added an inference engine as defined in the (inference_engine) module of the Rust-code and added two python-sided functions to bind the inference engine to the python code. 
+///             b. Increase the ability to customize the training of the data structure, were python-sided python functions were build to hookup into different steps of the preprocessing Rust pipeline
+///             c. increasing the documentation of the underlining library structure.
+///             
 /// NOTE:
 ///     THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
 ///     INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 ///     FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
 ///     
 ///     CHECK THE LICENSE FOR MORE DETAILS  
-/// Details:
-///     To be written 
-///-----------------------------------------------
-
-/// Exported functions
-/// ------------------
-/// The xxx library provides an interface to prepare and encode the datasets for training peptide HLA-II interaction and for running predictions on  
-/// the training datasets. The following functions are exported: 
-///     1.  generate_train_ds_shuffling_sm: which generate a training dataset made of negatives through shuffling of the positive peptides then
-///        numerically encodes it train and test dataset and then return two arrays, one representing the encoded sequences and representing the labels. 
-///         Further information can be obtained by calling the help function of the module.
-///     2.  
-///      
-///      
-/// 
-/// 
-/// 
-/// 
-/// 
-///   
-// Import the binder functions 
-//----------------------------
+///-----------------------------------------------------------------------------------------------------
 use pyo3::prelude::*;
-
 // Export the modules of the library
 //----------------------------------
 pub mod annotation_reader; 
@@ -66,7 +50,6 @@ use crate::pyhelper::*;
 use crate::inference_engine::*; 
 
 // add functions to the Py Module
-
 #[pymodule]
 fn OmLiT(_py: Python, m: &PyModule) -> PyResult<()> 
 {
@@ -87,8 +70,8 @@ fn OmLiT(_py: Python, m: &PyModule) -> PyResult<()>
     m.add_function(wrap_pyfunction!(group_peptide_by_protein, m)?)?;
     m.add_function(wrap_pyfunction!(group_by_9mers, m)?)?;
     m.add_function(wrap_pyfunction!(encode_sequence, m)?)?;
-    m.add_function(wrap_pyfunction!(encode_sequences,m)?);
-    m.add_function(wrap_pyfunction!(sample_negatives_from_positives_no_test,m)?); 
-
+    m.add_function(wrap_pyfunction!(encode_sequences,m)?)?;
+    m.add_function(wrap_pyfunction!(sample_negatives_from_positives_no_test,m)?)?; 
+    m.add_function(wrap_pyfunction!(annotate_and_encode_input_sequences,m)?)?; 
     Ok(())
 }
--- testing/test_sampling_link.py
+++ testing/test_sampling_link.py
@@ -11,16 +11,21 @@ from Bio import SeqIO
 ## Define the test variables 
 #---------------------------
 PATH2CASHED_DB='/Users/heshamelabd/projects/RustDB/datasets/cashed_database.db'
+PSEUDO_SEQ='/Users/heshamelabd/projects/RustDB/datasets/pseudosequence.2016.all.X.dat'
 FOLD_NEG=5
 THRESHOLD=1
 positive_peptides=dict()
 for seq in SeqIO.parse('/Users/heshamelabd/projects/RustDB/datasets/human_proteome_pos.fasta','fasta'):
     positive_peptides[seq.id]=str(seq.seq)
 
-input=([])
-
-linker.sample_negatives_from_positives_no_test((['SGASGPENFQVG']*10_000,['DRB1_1501;DRB_1301;1506']*10_000,['total PBMC']*10_000),
+try: 
+    results=linker.sample_negatives_from_positives_no_test((['SGASGPENFQVG']*1000,['DRB1_1501']*1000,['total PBMC']*1000),
 positive_peptides,PATH2CASHED_DB,FOLD_NEG,THRESHOLD)
+except Exception as exp:
+    raise RuntimeError(f"Calling the function failed due to the following error: {exp}")
 
-
+try:
+    (encoded_results,unmapped_results)=linker.annotate_and_encode_input_sequences(results,21,positive_peptides,PATH2CASHED_DB,PSEUDO_SEQ)
+except Exception as exp:
+    raise RuntimeError(f"Calling the function failed due to the following error: {exp}")
 
